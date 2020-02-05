55fa47320582f84aad06f42d7f34b4a53d1d1065
Michał Pietrasz <michal.pietrasz@gmail.com>
2020-02-05 21:21:09 +0100

    Store nickName in localStorage

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,6 +3,7 @@ import { withRouter } from "react-router";
 import useSocket from 'use-socket.io-client';
 import Login from './components/Login';
 import Chat from './components/Chat';
+import config from './config';
 import './App.scss';
 
 const ENDPOINT = process.env.REACT_APP_API_URL
@@ -13,9 +14,14 @@ const App = () => {
 
   function handleSetNickName (nickname: any) {
     setNickName(nickname);
+    localStorage.setItem(config.nickName, nickname);
   }
 
   useEffect(()=>{
+    const nickNameFromStorage = localStorage.getItem(config.nickName);
+    if (nickNameFromStorage) {
+      setNickName(nickNameFromStorage);
+    }
     socket.connect();
     socket.on('message', (data: any) => {
       console.log('incoming message', data)
---
+++ ui/src/config.ts
@@ -0,0 +1,5 @@
+const config = {
+    nickName: 'nickName',
+};
+
+export default config;
