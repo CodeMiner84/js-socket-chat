455a9d56829b962a801060e0e605ccdb5a9a279a
Michał Pietrasz <michal.pietrasz@gmail.com>
2020-02-08 18:16:15 +0100

    Emit room change from client

--- ui/src/components/Room/List.tsx
+++ ui/src/components/Room/List.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import RoomDto from './Room.dto';
+import {useChat} from "../../ChatContext";
 
-export default function List({ rooms }: any) {
+interface Props {
+  rooms: RoomDto[];
+}
+
+export default function List({ rooms }: Props) {
+  const chatContext = useChat();
+
+  const onRoomChange = (roomId: string) => {
+    chatContext.socket.emit('changeRoom', roomId);
+  }
+
   return (
     <div>
       <div>Rooms list:</div>
-      <ul>{rooms && rooms.map((room: RoomDto) => <li>{room.name}</li>)}</ul>
+      <ul>{rooms && rooms.map((room: RoomDto) => <li onClick={() => onRoomChange(room.id)}>{room.name}</li>)}</ul>
     </div>
   );
 }
