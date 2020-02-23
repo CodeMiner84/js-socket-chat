ee6caaecf1b7da2d5fee5332e5b83597a9c79f8c
Michał Pietrasz <michal.pietrasz@gmail.com>
2020-02-23 13:30:22 +0100

    Add redis config from env

--- api/src/redis.ts
+++ api/src/redis.ts
@@ -1,3 +1,7 @@
 import * as Redis from 'ioredis';
 
-export const redis = new Redis(6379, "redis://cache");
+export const redis = new Redis(
+  parseInt(process.env.REDIS_PORT, 10) || 6379,
+  process.env.REDIS_HOST || "redis://cache"
+);
+
