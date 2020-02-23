a2d6d1179ea1c13767a739b14417dd9c30d726ed
Michał Pietrasz <michal.pietrasz@gmail.com>
2020-02-23 13:55:09 +0100

    Redis adapter

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,29 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import * as Store from 'connect-redis';
-import * as session from 'express-session';
 import { EventsModule } from './events/events.module';
-import { redis } from './redis';
+import {redis} from './redis';
+import {RedisIoAdapter} from "./utils/redis.adapter";
 
 async function bootstrap() {
-  const RedisStore = Store(session);
   const app = await NestFactory.create(EventsModule);
-  // app.useWebSocketAdapter(new WsAdapter(app));
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
   app.enableCors();
-  app.use(
-    session({
-      store: new RedisStore({
-        client: redis as any,
-      }),
-      name: 'js-chat',
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        expires: true,
-        maxAge: 1000 * 60 * 60 * 24 * 365,
-      },
-    }));
+
   await app.listen(8080);
 }
 bootstrap();
