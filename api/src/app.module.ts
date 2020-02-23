1ba061274ee13f83bbf089361f3651e8f4bdbb8e
Michał Pietrasz <michal.pietrasz@gmail.com>
2020-02-23 13:19:05 +0100

    Remove unused module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { UserModule } from './user/user.module';
 import { RoomModule } from './room/room.module';
-import { CommonModule } from './common/common.module';
 import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [EventsModule, RoomModule, UserModule, CommonModule, MessageModule],
+  imports: [EventsModule, RoomModule, UserModule, MessageModule],
   providers: []
 })
 export class AppModule {}
