a1ae9dd038b173ca710c3808a643944ed49fc3a9
Michał Pietrasz <michal.pietrasz@gmail.com>
2020-02-24 21:33:52 +0100

    Remove RedisAdapter

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { EventsModule } from './events/events.module';
-import {redis} from './redis';
-import {RedisIoAdapter} from "./utils/redis.adapter";
 
 async function bootstrap() {
   const app = await NestFactory.create(EventsModule);
-  app.useWebSocketAdapter(new RedisIoAdapter(app));
   app.enableCors();
 
   await app.listen(8080);
--- api/src/redis.ts
+++ api/src/redis.ts
@@ -4,4 +4,3 @@ export const redis = new Redis(
   parseInt(process.env.REDIS_PORT, 10) || 6379,
   process.env.REDIS_HOST || "redis://cache"
 );
-
